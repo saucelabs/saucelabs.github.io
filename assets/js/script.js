cd1d55af29ee64dd22927e077ebbfac42506da2a
christian-bromann <mail@christian-bromann.com>
2020-06-18 14:42:54 +0200

    update hero graphic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const $ = window.$
-// Preloader js    
+// Preloader js
 $(window).on('load', function () {
     $('.preloader').fadeOut(100)
 })
@@ -9,11 +9,11 @@ $(document).ready(function () {
     const basePath = window.location.pathname.split('/')[1]
     const navigation = $('.navigation')
     const homePageLogo = $('#home-page-logo')
-    const contentPageLogo = $('#content-page-logo')    
+    const contentPageLogo = $('#content-page-logo')
     const navbar = $('.navbar')
     if (basePath.length > 0) {
         homePageLogo.remove()
-        contentPageLogo.removeClass('invisible')        
+        contentPageLogo.removeClass('invisible')
         navigation.removeClass('home-page')
         navigation.addClass('content-page')
         navbar.removeClass('home-page')
@@ -58,6 +58,56 @@ $(document).ready(function () {
         })
     })
 
+    window.onload = function () {
+        const parallaxBox = document.getElementById('parallax')
+
+        /*
+         * Fix js error, occurred at pages other than the home page.
+         * When there're no parallax, just ignore the below
+         * other operations, as below elements are bingding to the parallax.
+         */
+        if (!parallaxBox) {
+            return
+        }
+
+        const c2left = document.getElementById('l2').offsetLeft
+        const c2top = document.getElementById('l2').offsetTop
+        const c3left = document.getElementById('l3').offsetLeft
+        const c3top = document.getElementById('l3').offsetTop
+        const c4left = document.getElementById('l4').offsetLeft
+        const c4top = document.getElementById('l4').offsetTop
+        const c5left = document.getElementById('l5').offsetLeft
+        const c5top = document.getElementById('l5').offsetTop
+        const c6left = document.getElementById('l6').offsetLeft
+        const c6top = document.getElementById('l6').offsetTop
+        const c7left = document.getElementById('l7').offsetLeft
+        const c7top = document.getElementById('l7').offsetTop
+        const c8left = document.getElementById('l8').offsetLeft
+        const c8top = document.getElementById('l8').offsetTop
+
+        parallaxBox.onmousemove = function (event) {
+            event = event || window.event
+            const x = event.clientX - parallaxBox.offsetLeft
+            const y = event.clientY - parallaxBox.offsetTop
+
+            mouseParallax('l2', c2left, c2top, x, y, 25)
+            mouseParallax('l3', c3left, c3top, x, y, 20)
+            mouseParallax('l4', c4left, c4top, x, y, 35)
+            mouseParallax('l5', c5left, c5top, x, y, 30)
+            mouseParallax('l6', c6left, c6top, x, y, 45)
+            mouseParallax('l7', c7left, c7top, x, y, 30)
+            mouseParallax('l8', c8left, c8top, x, y, 25)
+        }
+    }
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        const obj = document.getElementById(id)
+        const parentObj = obj.parentNode
+        const containerWidth = parseInt(parentObj.offsetWidth)
+        const containerHeight = parseInt(parentObj.offsetHeight)
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px'
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px'
+    }
 
     // testimonial-slider
     $('.testimonial-slider').slick({
